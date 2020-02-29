c87602d6a61c3333aee6f32f1f4f498e3ba2979a
Randy Tate <randytatejr@gmail.com>
2020-02-29 13:36:01 -0600

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Howdy Y'all!!");
+            Console.ReadKey();
+
+            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
+            
+            var vowels = new[] { 'a', 'e', 'i', 'o', 'u', 'y' };
+            foreach (var animal in animals)
+            {
+                var numberOfVowels = 0;
+                foreach (var animalChar in animal)
+                {
+                    foreach (var vowel in vowels)
+                    {
+                        if (vowel == animalChar)
+                        {
+                            numberOfVowels++;
+                        }
+                    }
+                }
+                if (numberOfVowels >= 2)
+                {
+                    Console.WriteLine(animal);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
