d829cd0f0570390983b350f6035a921ab388eb82
Randy Tate <randytatejr@gmail.com>
2020-02-29 13:39:50 -0600

    testing

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Howdy Y'all!!");
-            Console.ReadKey();
+            Console.ReadKey(); 
 
             var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
             
