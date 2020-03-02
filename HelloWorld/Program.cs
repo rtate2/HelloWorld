c8fe707b6dd0e4c0d8a6ea958512cf9d7e249475
Randy Tate <randytatejr@gmail.com>
2020-03-01 20:52:57 -0600

    completed Hello World

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace HelloWorld
 {
@@ -6,10 +10,29 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Howdy Y'all!!");
-            Console.ReadKey(); 
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter parameter values.", "Canadian English", "North American English", "Australian English");
+                Console.Read();
+            }
+            else
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.Write(args[i] + Environment.NewLine);
+                }
+                Console.Read();
+            }
 
-            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
+            Console.WriteLine("Howdy Y'all!!");
+            Console.ReadKey();
+
+            var a = 10;
+            var b = 3;
+
+            Console.WriteLine(a > b);
+            
+            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan", "Bird" };
             
             var vowels = new[] { 'a', 'e', 'i', 'o', 'u', 'y' };
             foreach (var animal in animals)
